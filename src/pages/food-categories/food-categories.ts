0f977668ee99494951aadfa01c9bb8db9e698397
OU Sophea <ousophea@gmail.com>
2018-07-24 00:40:24 +0700

    Test REST API

--- src/pages/food-categories/food-categories.ts
+++ src/pages/food-categories/food-categories.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { RestProvider } from '../../providers/rest/rest';
 
 @IonicPage()
 @Component({
@@ -18,16 +19,28 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: 'food-categories.html',
 })
 export class FoodCategoriesPage {
-  categories: Observable<any>;
+  users: any;
+  errorMessage: string;
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public modalCtrl: ModalController,
-    private httpClient: HttpClient) {
-
-      this.categories = this.httpClient.get('http://127.0.0.1:8080/kiss-coffee.local/public/api/foods');  
-
+    private httpClient: HttpClient,
+    public rest: RestProvider 
+  ) {
+    
+      
+  }
+  ionViewDidLoad() {
+    this.getUsers();
   }
 
+  getUsers() {
+    this.rest.getUsers()
+    .then(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
 
   gotoCategoryItemList(category) {
     const modal = this.modalCtrl.create('FoodCategoryItemsPage', { category: category });
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
+
 
 /*
   Generated class for the RestProvider provider.
@@ -10,10 +14,66 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class RestProvider {
+  // private apiUrl = 'https://restcountries.eu/rest/v2/all';
+  // private apiUrlFoods = 'https://restcountries.eu/rest/v2/all';
+  apiUrl = 'http://www.dev-kiss.coffee.slor-ey.com/api/foods';
+  // apiUrl = 'https://jsonplaceholder.typicode.com';
 
+  token ='dIMf6WD79CdbeEfgjLAk1+a6AsV2l/4j/IdGyol66wY=';
   constructor(public http: HttpClient) {
-    console.log('Hello RestProvider Provider');
+    // console.log('Hello RestServiceProvider Provider');
+    this.getUsers();
+    let headers = new Headers({ 'Content-Type': 'application/json' });
   }
 
-  
+  getUsers(): Promise<any> {
+    let headers = new Headers(); 
+    headers.append('Content-Type', 'application/json'); 
+
+    return new Promise((resolve, reject) => {
+      this.http.get(this.apiUrl + '/users',{headers: headers})
+        .subscribe(
+          data => {
+            resolve(data);
+          }, err => {
+            console.log("Error in server-communication.ts: Getting objects failed: ");
+            console.log(err);
+            reject(err);
+          }
+        );
+    });
+  }
+
+  getHttpHeader() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization','Bearer ' + this.token);
+
+    // let options = new RequestOptions({ headers: headers });
+    return headers;
+  }
+
+  // getUsers() {
+
+  //   return new Promise(resolve => {
+  //     this.http.get(this.apiUrl + '/users').subscribe(data => {
+  //       resolve(data);
+  //     }, err => {
+  //       console.log(err);
+  //     });
+  //   });
+  // }
+  addUser(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl + '/users', JSON.stringify(data), {
+        headers: new HttpHeaders().set('Authorization', 'my-auth-token'),
+        params: new HttpParams().set('id', '3'),
+      })
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
 }
